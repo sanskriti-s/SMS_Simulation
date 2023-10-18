4b594487fda3b337e7e18f892a8049629a5e09c7
Sanskriti Sharma <sanskriti.r.sharma@gmail.com>
2023-10-18 14:04:42 -0400

    Changed individual time to avg time

--- progress1.py
+++ progress1.py
@@ -3,9 +3,14 @@ from time import sleep
 
 
 def progress_bar(j,sleep_time,num_recievers_per_sender,dropped):
+    total_time = 0
+    count = 0
     with Bar('Sender '+str(j)+' SMS sent', max=num_recievers_per_sender[j]) as bar:
         for i in range(num_recievers_per_sender[j]):
-            print('\nWait time: '+str(sleep_time[i][1]))
+            total_time +=sleep_time[i][1]
+            count += 1
+            mean_time = total_time/count
+            print('\nAverage wait time: '+str(mean_time))
             print('\nNum of dropped sms: '+str(dropped))
             sleep(sleep_time[i][1])
             bar.next()
