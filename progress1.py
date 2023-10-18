358d39cfe5bbfa671bf45291b1305b2ddbc3363f
Sanskriti Sharma <sanskriti.r.sharma@gmail.com>
2023-10-18 13:57:52 -0400

    output num of sms dropped

--- progress1.py
+++ progress1.py
@@ -2,11 +2,11 @@ from progress.bar import Bar
 from time import sleep
 
 
-def progress_bar(num_senders,sleep_time,num_recievers_per_sender):
-    for j in range(num_senders):
-        with Bar('Sender '+str(j)+' Packets sent', max=num_recievers_per_sender[j]) as bar:
-            for i in range(num_recievers_per_sender[j]):
-                print('\n'+str(sleep_time[j][i][1]))
-                sleep(sleep_time[j][i][1])
-                bar.next()
+def progress_bar(j,sleep_time,num_recievers_per_sender,dropped):
+    with Bar('Sender '+str(j)+' SMS sent', max=num_recievers_per_sender[j]) as bar:
+        for i in range(num_recievers_per_sender[j]):
+            print('\nWait time: '+str(sleep_time[i][1]))
+            print('\nNum of dropped sms: '+str(dropped))
+            sleep(sleep_time[i][1])
+            bar.next()
 
--- sender.py
+++ sender.py
@@ -4,28 +4,45 @@ import numpy as np
 import math
 from itertools import islice
 import concurrent.futures
+import random
 
-def per_sender(sleep_time,num_senders,num_per_sender):
-    progress_bar(num_senders,sleep_time,num_per_sender)
+def per_sender(i,sleep_time,num_per_sender,dropped):
+    progress_bar(i,sleep_time,num_per_sender,dropped)
 
 
 def send(num_senders, num_recievers, mess_len,mean_time_secs,std_time, fail_rate):
     pool = concurrent.futures.ThreadPoolExecutor()
     chunks =[]
+    dropped_sms = []
+    dropped = 0
     prod = produce(num_recievers,mess_len)
     unzip_prod = list(prod)
-    without_dropped_sms = unzip_prod[0:(len(unzip_prod) - math.ceil((len(unzip_prod)*fail_rate)))]
-    num_per_sender = [len(without_dropped_sms) // num_senders + (1 if x < len(without_dropped_sms) % num_senders else 0)  for x in range (num_senders)]
-    time_dist = np.random.normal(mean_time_secs, std_time,len(without_dropped_sms))   
 
+    failed_sms = math.ceil((len(unzip_prod))*fail_rate)
+    without_dropped_sms = unzip_prod.copy()
+    for _ in range(failed_sms):
+        random_element = random.choice(without_dropped_sms)
+        dropped_sms.append(random_element)
+        without_dropped_sms.remove(random_element)
 
-    new_zip = zip(without_dropped_sms,time_dist)
+    num_per_sender = [len(unzip_prod) // num_senders + (1 if x < len(unzip_prod) % num_senders else 0)  for x in range (num_senders)]
+    time_dist = np.random.normal(mean_time_secs, std_time,len(unzip_prod))   
+
+    new_zip = zip(unzip_prod,time_dist)
     it = iter(list(new_zip))
     chunks.append([list(islice(it, 0, j)) for j in num_per_sender])
-    pool.submit(per_sender(chunks[0],num_senders,num_per_sender))
+    
+    for i in range(num_senders):    
+        for j in range(len(chunks[0][i])):
+            if(chunks[0][i][j][0] in dropped_sms):
+                print("SMS dropped")
+                dropped += 1
+            else:
+                pool.submit(per_sender(i,chunks[0][i],num_per_sender, dropped))
+                break
     pool.shutdown(wait=True)
  
-    print("All senders completed")
+    print("All senders completed.")
 
 send(5,10,100,5,0.3,0.2)
     
